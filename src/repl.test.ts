00f9583b2b711fe473fa926045a0cba529d6613a
gooneraki <hayiate4@hotmail.com>
2025-09-25 14:59:20 +0300

    feat: restructure command handling by introducing State management and updating command functions

--- src/repl.test.ts
+++ src/repl.test.ts
@@ -1,29 +1,28 @@
-import { cleanInput } from "./repl";
+import { cleanInput } from "./repl.js";
 import { describe, expect, test } from "vitest";
 
 describe.each([
-  {
-    input: "  hello  world  ",
-    expected: ["hello", "world"],
-  },
   {
     input: "  ",
     expected: [],
   },
   {
-    input: " HELLO  pretty boy  ",
-    expected: ["hello","pretty","boy"],
+    input: "  hello  ",
+    expected: ["hello"],
   },
-  
+  {
+    input: "  hello  world  ",
+    expected: ["hello", "world"],
+  },
+  {
+    input: "  HellO  World  ",
+    expected: ["hello", "world"],
+  },
 ])("cleanInput($input)", ({ input, expected }) => {
   test(`Expected: ${expected}`, () => {
-    const actual = cleanInput(input)
-
-    // The `expect` and `toHaveLength` functions are from vitest
-    // they will fail the test if the condition is not met
+    const actual = cleanInput(input);
     expect(actual).toHaveLength(expected.length);
     for (const i in expected) {
-      // likewise, the `toBe` function will fail the test if the values are not equal
       expect(actual[i]).toBe(expected[i]);
     }
   });
