db61ded0e96ec581c73ce4255e20014635fd45e1
gooneraki <hayiate4@hotmail.com>
2025-09-25 17:11:10 +0300

    feat: add commandExplore for location exploration and update command handling

--- src/commands.ts
+++ src/commands.ts
@@ -3,6 +3,7 @@ import { commandExit } from "./command_exit.js";
 import { commandMapForward, commandMapBack } from "./command_map.js";
 
 import type { CLICommand } from "./state.js";
+import { commandExplore } from "./command_explore.js";
 
 export function getCommands(): Record<string, CLICommand> {
   return {
@@ -25,6 +26,11 @@ export function getCommands(): Record<string, CLICommand> {
       name: "mapb",
       description: "Get the previous page of locations",
       callback: commandMapBack,
+    },
+    explore: {
+      name: "explore <location_name>",
+      description: "Explore a location",
+      callback: commandExplore,
     },
   };
 }
